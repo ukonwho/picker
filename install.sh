9d6918c2bbd2dc941bff238091036b6612f68b09
lucius <suicul@gmail.com>
2024-07-28 22:20:30 +0800

    modify

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 python3 -m pip install -r requirements.txt
 
-sudo apt-get install -y wget
+#sudo apt-get install -y wget
 #wget -q https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0-rc2/go-cqhttp_linux_amd64.tar.gz -O ./cqhttp/go-cqhttp.tar.gz
-wget -q https://github.com/Mrs4s/go-cqhttp/releases/download/v1.2.0/go-cqhttp_linux_amd64.tar.gz -O ./cqhttp/go-cqhttp.tar.gz
+#wget -q https://github.com/Mrs4s/go-cqhttp/releases/download/v1.2.0/go-cqhttp_linux_amd64.tar.gz -O ./cqhttp/go-cqhttp.tar.gz
+sudo apt install curl
+curl -sL https://github.com/Mrs4s/go-cqhttp/releases/download/v1.2.0/go-cqhttp_linux_amd64.tar.gz -o ./cqhttp/go-cqhttp.tar.gz
 cd cqhttp && tar xzf go-cqhttp.tar.gz go-cqhttp && rm go-cqhttp.tar.gz
